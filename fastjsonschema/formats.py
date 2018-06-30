01217c4af3234d9c4f2ea45e78a94cddfc8dd68c
Antti Jokipii <anttijokipii@gmail.com>
2018-06-30 19:05:58 +0300

    valid time regexp

--- fastjsonschema/formats.py
+++ fastjsonschema/formats.py
@@ -15,12 +15,12 @@ from .exceptions import JsonSchemaException
 
 
 DATE_REGEX = re.compile(
-    r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})$'
+    r'^(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})$'
 )
 
 TIME_REGEX = re.compile(
-    r'(?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
-    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
+    r'^(?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
+    r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6}))?([zZ]|[+-]\d\d:\d\d)?)?$'
 )
 
 DATETIME_REGEX = re.compile(ISO_8601_DATETIME)
