dd74828a2633177a307eba139082956832e3cc3e
Antti Jokipii <anttijokipii@gmail.com>
2018-07-03 01:48:14 +0300

    test_compile_to_code... more tests

--- tests/test_compile_to_code.py
+++ tests/test_compile_to_code.py
@@ -1,23 +1,20 @@
-import os
-import importlib
 import pytest
 
 from fastjsonschema import JsonSchemaException, compile_to_code
 
 
-def test_compile_to_code():
-    name, code = compile_to_code({
-        'properties': {
+exc = JsonSchemaException('data.a must be string')
+@pytest.mark.parametrize('value, expected', [
+    ({'a': 'a', 'b': 1}, {'a': 'a', 'b': 1}),
+    ({'a': 1, 'b': 1}, exc),
+])
+def test_compile_to_code(asserter_cc, value, expected):
+    asserter_cc(
+        {'properties': {
             'a': {'type': 'string'},
             'b': {'type': 'integer'},
-        }
-    })
-    if not os.path.isdir('temp'):
-        os.makedirs('temp')
-    with open('temp/schema.py', 'w') as f:
-        f.write(code)
-    validate = getattr(importlib.import_module('temp.schema'), name)
-    assert validate({'a': 'a', 'b': 1}) == {'a': 'a', 'b': 1}
+        }}, value, expected
+    )
 
 exc = JsonSchemaException('data.a must match pattern [ab]')
 @pytest.mark.parametrize('value, expected', [
